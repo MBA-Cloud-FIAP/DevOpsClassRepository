f25659b7bfe74d04a8396735a3d363301f3f05b9
Joao Pedro <joaopedroprog@outlook.com>
2025-01-15 18:29:39 -0300

    Fix start_server.sh file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 cd /home/ec2-user/app
 
-if pgrep -f "python manage.py runserver"; then
-  pkill -f "python manage.py runserver"
+if pgrep -f "python3 app.py"; then
+  pkill -f "python3 app.py"
 fi
 
-nohup python3 manage.py runserver 0.0.0.0:8000 &
+nohup python3 app.py > flask.log 2>&1 &
